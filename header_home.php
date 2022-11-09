4ce1b722a6afbe6c4410555cfa7d8da556a6d313
hieuaka47 <102672810+hieuaka47@users.noreply.github.com>
2022-11-09 07:36:43 +0700

    suong qua

---
+++ header_home.php
@@ -0,0 +1,23 @@
+<?php
+    echo 
+    '<header class="header">
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+        <a href="#" class="logo"> <span>Huy</span> Idol </a>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#phones">phones</a>
+            <a href="#cart">cart</a>
+            <a href="#featured">featured</a>
+            <a href="#reviews">reviews</a>
+            <a href="#contact">contact</a>
+        </nav>
+
+        <div id="login-btn">
+            <button class="btn">login</button>
+            <i class="far fa-user"></i>
+        </div>
+
+    </header>';
